baf729c564b72b77d99f926bcee03f92161e1257
1r2o3b4e5l <robelamsalu44@gmail.com>
2024-10-03 10:34:38 -0700

    0x02-ES6_classes

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+export default class Building {
+  constructor(sqft) {
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() {
+    if (this.constructor.name !== 'Building') throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
